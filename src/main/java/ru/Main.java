964ff162c807f21d747e625e879688ff4cbbd85e
Nikolay Korobeynikov <nickkorobeynikov@gmil.com>
2026-05-25 21:43:40 +0300

    initial

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -3,6 +3,7 @@ package ru;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, Денис!");
+        System.out.println("initial commit");
         System.out.println("Hello world");
     }
 }
