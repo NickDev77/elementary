01f9987ee0519b8c052599d650a9861d437aca62
Nikolay Korobeynikov <nickkorobeynikov@gmil.com>
2026-05-26 21:20:50 +0300

    task 1.6 methods and arguments

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -1,9 +1,15 @@
 package ru;
 
+import ru.calculator.Calculator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, Денис!");
         System.out.println("initial commit");
         System.out.println("Hello world");
+
+        Calculator.plus(100, 500);
+        Calculator.plus(4, 2);
+        Calculator.plus(3, 5);
     }
 }
--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -2,23 +2,16 @@ package ru.calculator;
 
 public class Calculator {
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
+        plus(100, 500);
+        plus(4, 2);
+        plus(3, 5);
+    }
 
-        int six = 6;
-        int four = 4;
-        int five = 5;
-
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+    public static void plus(int num1, int num2) {
+        int result = num1 + num2;
+        System.out.println(result);
     }
 }
+
 
 
