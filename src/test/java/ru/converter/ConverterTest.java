4c0746dd85951329c22cbbb38b12322e50916b18
Nikolay Korobeynikov <nickkorobeynikov@gmil.com>
2026-05-29 21:06:02 +0300

    fix checkstyle

--- src/test/java/ru/converter/ConverterTest.java
+++ src/test/java/ru/converter/ConverterTest.java
@@ -25,8 +25,7 @@ class ConverterTest {
         float eps = 0.0001f;
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
-
-
+    
     @Test
     void whenConvert180RblThen2Dlr() {
         float in = 180;
