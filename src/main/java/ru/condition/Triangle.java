0dae386e4bdf2643c9a1b446d87b34e37b5cd2fb
Nikolay Korobeynikov <nickkorobeynikov@gmil.com>
2026-06-01 23:01:09 +0300

    task 3.13 triangle task comparing

---
+++ src/main/java/ru/condition/Triangle.java
@@ -0,0 +1,12 @@
+package ru.condition;
+
+public class Triangle {
+    public static boolean exist(double ab, double ac, double bc) {
+        return ab + ac > bc && ab + bc > ac && ac + bc > ab;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(exist(3, 4, 5));
+        System.out.println(exist(1, 1, 2));
+    }
+}
