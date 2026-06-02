9e174259413193e75d6127f5591d4ffdfe8e39ff
Nikolay Korobeynikov <nickkorobeynikov@gmil.com>
2026-06-02 20:54:45 +0300

    task 3.16 has been fixed after bug

--- src/main/java/ru/condition/ThreeMax.java
+++ src/main/java/ru/condition/ThreeMax.java
@@ -6,10 +6,10 @@ public class ThreeMax {
         if (first > second && first > third) {
             result = first;
         }
-        if (second > first && second > third) {
+        if (second >= first && second > third) {
             result = second;
         }
-        if (third > first && third > second) {
+        if (third >= first && third >= second) {
             result = third;
         }
         return result;
